bb7271594f408ad01d3dd3ff5f933350bffc8c49
angelalvess <angeltrashangel@gmail.com>
2024-03-29 17:33:35 -0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
